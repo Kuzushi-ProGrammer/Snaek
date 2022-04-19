b9de868561dd07c0caf1df291d466b8b9b41236b
Dominique Spavor <Derp.rosy@gmail.com>
2022-04-19 16:45:52 -0600

    Add project files.

---
+++ Snaek/Snaek.py
@@ -0,0 +1,12 @@
+# Start player at origin position
+# Set a direction
+# Draw a square in that direction
+# Check for input
+# Change direction if nessesary
+
+# Spawn 'apple'
+# Check if player runs over apple
+# Make player longer
+
+
+
