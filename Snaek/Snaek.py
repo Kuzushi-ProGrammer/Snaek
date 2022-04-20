bf2ac8f9b6aeeec7eb0c3cfb1439c09c2258ce49
Dominique Spavor <Derp.rosy@gmail.com>
2022-04-20 00:03:09 -0600

    Basic grid movement implemented (Moving from coord to coord)

--- Snaek/Snaek.py
+++ Snaek/Snaek.py
@@ -12,7 +12,7 @@
 
 # If player runs over square, die
 # If player runs into wall, die
-import pygame, sys, os, pygame_menu
+import pygame, sys, os, pygame_menu, time
 from pygame.locals import *
 
 black = (0, 0, 0)
@@ -34,55 +34,95 @@ screen = pygame.display.set_mode(window)
 
 
 def main():
-   print('Balls')
-   screen.fill(black)
-   n = 25
-   f = 25
+    print('Balls')
+    screen.fill(black)
 
-   # unsure if there is a better way to do this
-   snakemap = {
-       "A1": {
-           "occupied": False,
-           "coordinates": (0, 0)                     # Taking top left of every square (squares are 25x25)
-           },
-       "A2": {
-           "occupied": False,
-           "coordinates": (26, 0)                     # Taking top left of every square (squares are 25x25)
-           },
-       "A3": {
-           "occupied": False,
-           "coordinates": (52, 0)                     # Taking top left of every square (squares are 25x25)
-           },
-       "A4": {
-           "occupied": False,
-           "coordinates": (78, 0)                     # Taking top left of every square (squares are 25x25)
-           },
-       "A5": {
-           "occupied": False,
-           "coordinates": (104, 0)                     # Taking top left of every square (squares are 25x25)
-           },
-       }
+    n = 25
+    f = 25
 
-   # The range of the board is 20x20 grid, which means the coords are in range A-T and 1-20
+    Xtuple = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T")
+    Ytuple = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19")
 
-   # 25 pixel squares?
-   # getting a feel for the data
-   for s in range(20): # x axis
+    print(Xtuple[12] + Ytuple[12]) # THIS WORKS!
+
+    # The range of the board is 20x20 grid, which means the coords are in range A-T and 1-20
+
+    # 25 pixel squares?
+    # getting a feel for the data
+    for s in range(20): # x axis
         pygame.draw.rect(screen, white, (0, n, y, 1))
         n += 25
 
-   for m in range(20): # y axis
+    for m in range(20): # y axis
         pygame.draw.rect(screen, white, (f, 0, 1, x))
         f += 25
 
-   running = True
-   while running:
+    xx = 12
+    yy = 12
+
+    direction = "UP"
+    position = Xtuple[xx] + Ytuple[yy]
+
+    running = True
+    while running:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         pygame.display.update()
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_UP] or key[pygame.K_w]:
+            direction = "UP"
+
+        elif key[pygame.K_DOWN] or key[pygame.K_s]:
+            direction = "DOWN"
+
+        elif key[pygame.K_LEFT] or key[pygame.K_a]:
+            direction = "LEFT"
+
+        elif key[pygame.K_RIGHT] or key[pygame.K_d]:
+            direction = "RIGHT"
+
+
+        if direction == "UP":
+            yy += 1
+            try:
+                print(Xtuple[xx] + Ytuple[yy])
+            except:
+                print("death")
+                pass
+            time.sleep(0.5)
+
+        if direction == "DOWN":
+            yy -= 1
+            try:
+                print(Xtuple[xx] + Ytuple[yy])
+            except:
+                print("death")
+                pass
+            time.sleep(0.5)
+
+        if direction == "LEFT":
+            xx += 1
+            try:
+                print(Xtuple[xx] + Ytuple[yy])
+            except:
+                print("death")
+                pass
+            time.sleep(0.5)
+
+        if direction == "RIGHT":
+            xx -= 1
+            try:
+                print(Xtuple[xx] + Ytuple[yy])
+            except:
+                print("death")
+                pass
+            time.sleep(0.5)
+
+        
 
 def menu():
 
