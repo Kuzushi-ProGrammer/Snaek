7dd9bf17f797126f70350ebdee83c9869a6c88e6
Dominique Spavor (Kuzushi) <derp.rosy@gmail.com>
2022-04-25 15:27:56 -0600

    More work on snake itself

--- Snaek/Snaek.py
+++ Snaek/Snaek.py
@@ -20,9 +20,11 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 snake = white                                     # potential color swap feature implementation
+snakelen = 3
 
 dirlist = ["UP", "UP"]                            # assigning list and default value
 poslist = []
+coordlist = []
 
 window = (500, 500)
 (x, y) = window
@@ -36,49 +38,23 @@ screen = pygame.display.set_mode(window)
 
 # ---- Main Function ---- #
 def main():
-    print('Balls')
     screen.fill(black)
 
     global dirlist
     global poslist
-
-    n = 25
-    f = 25
+    global coordlist
+    global snakelen
 
     Xtuple = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T")
     Ytuple = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19")
 
     # The range of the board is 20x20 grid, which means the coords are in range A-T and 1-20
-    '''
-    # 25 pixel squares
-    for s in range(20): # x axis
-        pygame.draw.rect(screen, white, (0, n, y, 1))
-        n += 25
-
-    for m in range(20): # y axis
-        pygame.draw.rect(screen, white, (f, 0, 1, x))
-        f += 25
-    '''
-
+    
     xx = 12
     yy = 12
 
     direction = "UP"                                    # Default Direction
     position = Xtuple[xx] + Ytuple[yy]                  # Outputs coord on grid (ex. M12)
-
-    '''
-    testl = []
-    testl.append("bitches")                             # Testing list inserts
-    testl.append("and bros")
-    testl.append("and non binary hoes")
-
-    print(testl)
-
-    testl.insert(0, "women")
-    testl.pop(2)
-
-    print(testl)
-    '''
 
 # ---- Main gameloop ---- #
     running = True                                      # Game loop started
@@ -117,63 +93,68 @@ def main():
         prevdir = dirlist[1]
 
 # ---- Movement On Grid ---- #
-        delay = 0.2
+        delay = 0.1
+
         if direction == "UP":                       # Constantly moving the snake on the coordinates logic (delay so it's managable and more like snake)
             if prevdir == "DOWN":                   # If up and if prevdir doesnt equal down go up
                 yy += 1
-                time.sleep(delay)
             else:
                 yy -= 1
-                time.sleep(delay)
-            #try:
-                #print(Xtuple[xx] + Ytuple[yy])     # Debug and also OOB ( indicator
-           # except:
-               # print("death")
-               # pass
                 
         elif direction == "DOWN":                   
             if prevdir == "UP":                     # If the pressed direction is down and the previous direction is up, keep going up
                 yy -=1                              
-                time.sleep(delay)
             else:
                 yy += 1                             
-                time.sleep(delay)
 
         elif direction == "LEFT":                  
             if prevdir == "RIGHT":
                 xx += 1                             # Right (stops player from going back on themselves)
-                time.sleep(delay)
             else:
                 xx -= 1                             # Left (direction they will go if not going backwards)
-                time.sleep(delay)
 
         elif direction == "RIGHT":                
             if prevdir == "LEFT":
                 xx -= 1                             
-                time.sleep(delay)
             else:
                 xx += 1                             
-                time.sleep(delay)
 
-# ---- Position Storing ---- # 
-        poslist.append(Xtuple[xx] + Ytuple[yy])
-        print(poslist)
-        
-        # need to track how long snake is so that the game can delete the squares outside the length
+        time.sleep(delay)
 
-# hear me out, variable * 25 + 1 (for calculating square coords) (ex. xx = 7 so 7 * 25 is 175 + 1 = 176) 
-# (then just add 25 to coords to make square)
-        xcoord = (xx * 25 + 1)
-        ycoord = (yy * 25 + 1)
-
+# ---- Position Storing ---- #
+        xcoord = (xx * 25)
+        ycoord = (yy * 25)
         coords = (xcoord, ycoord)
 
-        pygame.draw.rect(screen, white, (xcoord, ycoord, 25, 25))
+        coordlist.insert(0, coords)
+        print(coordlist)
 
-        # make some form of list that tracks (Xtuple[xx] + Ytuple[yy}) (ex. M12, M13, L13, etc.)
-        # storing the values that the snaek moved essentially
+        poslist.insert(0, Xtuple[xx] + Ytuple[yy])
+        print(poslist)
 
+        head = coordlist[0]
+        tail = coordlist[snakelen]
+   
+        # need a way to determine end of tail (currently not working rn
+        # stops rendering after 3rd pos
+        # render head, unrender tail?
+        for x in poslist:
+            xindex = poslist.index(x)
+            if xindex > snakelen:
+                pygame.draw.rect(screen, white, (head, 25, 25))
+                pygame.draw.rect(screen, (69, 69, 69), (tail, 25, 25))
+                poslist.pop(snakelen)
+                coordlist.pop(snakelen)
+            elif xindex <= snakelen:
+                pygame.draw.rect(screen, white, (xcoord, ycoord, 25, 25))
+
+        poslist = poslist[0:snakelen]
         
+        # Need a coord list for all the parts
+        
+        # if coord equals negative:
+            # die
+ 
 
 def menu():
 
